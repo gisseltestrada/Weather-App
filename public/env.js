a3d6ef630d12fe301bbc2383b2e09ba7bd63451c
gisselt <84683137+gisseltestrada@users.noreply.github.com>
2022-04-23 16:19:43 -0500

    first commit

---
+++ public/env.js
@@ -0,0 +1,5 @@
+window.env = {
+  "apiKey": "fce9328f7e0f0e1d0e315cbd9e688a0c",
+  "fiveDayApi": "api.openweathermap.org/data/2.5/forecast?",
+  "currentApi": "https://api.openweathermap.org/data/2.5/weather?"
+};
