21d15f708b9d2ea3b71a26bff467a41f8b00c9ce
gisselt <84683137+gisseltestrada@users.noreply.github.com>
2022-05-05 20:36:56 -0500

    last commit

--- public/env.js
+++ public/env.js
@@ -1,6 +1,5 @@
 window.env = {
-  "apiKey": "0ce249d08a8d15ad82be9ce670c9c5f3",
-  "fiveDayApi": "https://api.openweathermap.org/data/2.5/forecast",
+  "apiKey": "dd8c49e8886ccc007f08fb4a56af1925",
   "currentApi": "https://api.openweathermap.org/data/2.5/weather",
   "sevenDayApi": "https://api.openweathermap.org/data/2.5/onecall",
   "geocodeApi": "http://api.openweathermap.org/geo/1.0/direct"
